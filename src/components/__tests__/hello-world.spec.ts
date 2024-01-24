06606a52441bfc0c4ea06731ca30075422df4075
KitanoNani <736818560@qq.com>
2024-01-25 00:00:11 +0800

    fix: type fix

--- src/components/__tests__/hello-world.spec.ts
+++ src/components/__tests__/hello-world.spec.ts
@@ -5,7 +5,13 @@ import HelloWorld from '../common/hello-world.vue'
 
 describe('HelloWorld', () => {
   it('renders properly', () => {
-    const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitest' } })
-    expect(wrapper.text()).toContain('Hello Vitest')
+    const msgTest = {
+      version: new Date(''),
+      versionText: '这只是一个测试',
+      welcomeText: '我就问我就问，我到底要怎么做你们才能放过我'
+    }
+    const wrapper = mount(HelloWorld, { props: { msg: msgTest } })
+    expect(wrapper.text()).toContain('我就问我就问，我到底要怎么做你们才能放过我')
+    expect(wrapper.text()).toContain('这只是一个测试')
   })
 })
